53812b3c93da0ae3a3de44796584aaef200d059d
Daniela Schiano di Cola <daschi87@gmail.com>
2019-05-11 10:59:24 -0700

    Iterate on thoughts and notes

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS uuid-ossp;
 
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users (
   user_id       uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
   email         text UNIQUE,
@@ -8,53 +9,73 @@ CREATE TABLE IF NOT EXISTS users (
   avatar_url    text
 );
 
-CREATE TABLE IF NOT EXISTS versions (
-  verion_id     text PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-  previous_id   text,
-  created_at    timestamp NOT NULL
+DROP TABLE IF EXISTS gists CASCADE;
+CREATE TABLE IF NOT EXISTS gists (
+  gist_id       uuid      PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  user_id       text      REFERENCES users (user_id)
+  -- head          text      REFERENCES revisions (revision_id),
+  name          text,
+  description   text,
+  private       boolean   DEFAULT FALSE
 );
 
+DROP TABLE IF EXISTS revisions CASCADE;
+CREATE TABLE IF NOT EXISTS revisions (
+  revision_id   text PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  gist_id       text REFERENCES gists (gist_id),
+  -- must reference the latest revision_id
+  previous_id     text REFERENCES revisions (revision_id),
+  created_at    timestamp NOT NULL DEFAULT now()
+
+);
+
+-- Unique on created_at would also solve this issue
+
+CREATE UNIQUE INDEX first_revision ON revisions USING btree (id) WHERE (parent_id IS NULL);
+
+
+CREATE UNIQUE INDEX subsequent_revision ON revisions USING btree (gist_id, parent_id);
+
+DROP TABLE IF EXISTS files CASCADE;
 CREATE TABLE IF NOT EXISTS files (
   file_id       text PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-  gist_id       text REFERENCES gists (uuid),
-  previous_id   text,
+  gist_id       text REFERENCES gists (gist_id),
+  -- Do we still need previous_id here if we know which revision this
+  -- file is a part of, and then can diff the content between this
+  -- file and the file from the previously created revision ?
+  -- previous_id     text,
   filename      text NOT NULL,
   content       text NOT NULL,
   diff          text
 );
 
-CREATE TABLE IF NOT EXISTS version_files (
-  version_id text REFERENCES versions (version_id),
-  file_id   text REFERENCES files (file_id),
-  PRIMARY KEY (version_id, file_id),
+DROP TABLE IF EXISTS revision_files CASCADE;
+CREATE TABLE IF NOT EXISTS revision_files (
+  revision_id   text REFERENCES revisions (revision_id),
+  file_id       text REFERENCES files (file_id),
+  PRIMARY KEY (revision_id, file_id),
 );
 
-CREATE TABLE IF NOT EXISTS gists (
-  gist_id     uuid      PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-  user_id     text      REFERENCES users (user_id)
-  head        text      REFERENCES versions (version_id),
-  name        text,
-  description text,
-  private     boolean
-);
-
+DROP TABLE IF EXISTS comments CASCADE;
 CREATE TABLE IF NOT EXISTS comments (
-  comment_id text PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-  gist_id text REFERENCES gists (gist_id),
-  user_id text REFERENCES users (user_id),
-  content text NOT NULL,
-  created_at timestamp NOT NULL,
-  updated_at timestamp NOT NULL
+  comment_id    text PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  gist_id       text REFERENCES gists (gist_id),
+  user_id       text REFERENCES users (user_id),
+  content       text NOT NULL,
+  created_at    timestamp NOT NULL DEFAULT now(),
+  updated_at    timestamp NOT NULL DEFAULT now()
 );
 
+DROP TABLE IF EXISTS subscriptions CASCADE;
 CREATE TABLE IF NOT EXISTS subscriptions (
-  gist_id text REFERENCES gists (uuid),
-  user_id text REFERENCES users (uuid),
+  gist_id       text REFERENCES gists (uuid),
+  user_id       text REFERENCES users (uuid),
   PRIMARY KEY (gist_id, user_id)
 );
 
+DROP TABLE IF EXISTS stars CASCADE;
 CREATE TABLE IF NOT EXISTS stars (
-  gist_id text REFERENCES gists (uuid),
-  user_id text REFERENCES users (uuid),
+  gist_id       text REFERENCES gists (uuid),
+  user_id       text REFERENCES users (uuid),
   PRIMARY KEY (gist_id, user_id)
 );
--- seed.sql
+++ seed.sql
@@ -1,4 +1,22 @@
-INSERT INTO users VALUES (10, "hello@dataquest.io", "Some Name", "123 Fake St.")
-INSERT INTO users VALUES (10, "hello@dataquest.io", "Some Name", "123 Fake St.")
-INSERT INTO users VALUES (10, "hello@dataquest.io", "Some Name", "123 Fake St.")
-INSERT INTO users VALUES (10, "hello@dataquest.io", "Some Name", "123 Fake St.")
+-- event-based modeling
+-- by the way, this kind of iterative approach towards a more event-based modeling is what i was trying to allude to in the seminar. really an `ItemPricing` is an event where we as a business have decided to change the price of something. thinking about these kinds of events from the beginning can lead to more abstract but more flexible and robust models.
+
+-- What events are in this system and how might we model them?
+-- revision event
+
+-- commands: CRUD gists
+-- queries:
+  -- get latest version of gist
+  -- get paginated versions of gist
+  -- view a file from latest version
+  -- view a file from versions
+  -- list a gist's comments
+  -- get count of gist's subscriptions
+  -- get count of gist's stars
+--
+-- a gist has many revisions
+-- a revision belongs to a gist
+-- a gist references the most recent revision
+
+-- a revision has many files
+-- a file belongs to a revision
